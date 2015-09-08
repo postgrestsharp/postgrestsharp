b13ae829c0cf3b13344b0d4e247cf39a621bb14c
Eugene Duvenage <eugened@sahomeloans.com>
2015-09-08 11:47:39 +0200

    raml proccessor change

--- src/PostgRESTSharp/Commands/GenerateRAML/GenerateRAMLCommandProcessor.cs
+++ src/PostgRESTSharp/Commands/GenerateRAML/GenerateRAMLCommandProcessor.cs
@@ -1,15 +1,15 @@
 ﻿using PostgRESTSharp.Commands.GenerateViewScripts.Templates;
 using System.Collections.Generic;
 using System.IO;
+using System;
 
 namespace PostgRESTSharp.Commands.GenerateRAML
 {
     public class GenerateRAMLCommandProcessor : CommandProcessor, IGenerateRAMLCommandProcessor
     {
-        public void Process(IEnumerable<IViewMetaModel> views, int viewSchemaVersion, string fileName, string outputDirectory)
+        public void Process(string baseURI, string title, IEnumerable<IViewMetaModel> views, int viewSchemaVersion, string fileName, string outputDirectory)
         {
-            // generate the RAML
-
+            throw new NotImplementedException();
         }
     }
 }
--- src/PostgRESTSharp/Commands/GenerateRAML/IGenerateRAMLCommandProcessor.cs
+++ src/PostgRESTSharp/Commands/GenerateRAML/IGenerateRAMLCommandProcessor.cs
@@ -5,7 +5,7 @@ namespace PostgRESTSharp.Commands.GenerateRAML
 {
 	public interface IGenerateRAMLCommandProcessor
     {
-		void Process(IEnumerable<IViewMetaModel> views, int viewSchemaVersion, string fileName, string outputDirectory);
+		void Process(string baseURI, string title, IEnumerable<IViewMetaModel> views, int viewSchemaVersion, string fileName, string outputDirectory);
 	}
 }
 
