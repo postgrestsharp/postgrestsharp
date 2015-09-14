ed2a9cd7fffafd021cb4729f97d19effae79de5b
Leigh Boyter <LeighB@sahomeloans.com>
2015-09-14 07:18:15 +0200

    updated responses

--- src/PostgRESTSharp/Commands/GenerateRAML/Maps/ResponseMapping.cs
+++ src/PostgRESTSharp/Commands/GenerateRAML/Maps/ResponseMapping.cs
@@ -20,10 +20,6 @@ namespace PostgRESTSharp.Commands.GenerateRAML.Maps
                 expression => expression.MapFrom(src => (int)src.StatusCode)
             )
             .ForMember(
-                model=>model.Description,
-                expression => expression.MapFrom(src=>src.ResponseSchema)
-            )
-            .ForMember(
                 model => model.Body,
                 expression => expression.MapFrom(src => new Dictionary<string,RESTResponseDefinition>(){
                     {"application/javascript",src}
