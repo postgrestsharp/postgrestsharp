7f50974b51663dcc508aa0286181e0fee886bcdc
Eugene Duvenage <eugened@sahomeloans.com>
2015-10-06 21:39:39 +0200

    fixed view generation for tables that have no roles granted

--- src/PostgRESTSharp/Commands/GenerateViewScripts/Templates/ViewScript2.cs
+++ src/PostgRESTSharp/Commands/GenerateViewScripts/Templates/ViewScript2.cs
@@ -26,15 +26,23 @@ namespace PostgRESTSharp.Commands.GenerateViewScripts.Templates
 
         public string GetSources(IViewMetaModel view)
         {
-            return string.Format("{0} {1} \n", view.PrimarySource.TableName, view.PrimarySource.TableName.Replace("$", "_")) + 
-                string.Join("\n", view.JoinSources.Select(x => string.Format("JOIN {0} {1} ON {2}.{3} = {1}.{4}", 
-                    x.JoinSource.TableName, x.JoinSource.TableName.Replace("$", "_"), 
-                    x.Source.TableName.Replace("$", "_"), x.SourceColumn.ColumnName, x.JoinColumn.ColumnName)));
+			if (view.JoinSources.Count () > 0) {
+				return string.Format ("{0} {1} \n", view.PrimarySource.TableName, view.PrimarySource.TableName.Replace ("$", "_")) +
+				string.Join ("\n", view.JoinSources.Select (x => string.Format ("JOIN {0} {1} ON {2}.{3} = {1}.{4}", 
+					x.JoinSource.TableName, x.JoinSource.TableName.Replace ("$", "_"), 
+					x.Source.TableName.Replace ("$", "_"), x.SourceColumn.ColumnName, x.JoinColumn.ColumnName)));
+			} else {
+				return string.Format ("{0} \n", view.PrimarySource.TableName);
+			}
         }
+
+		public bool HasWhereClause(IViewMetaModel view)
+		{
+			return view.FilterElements.Count () != 0;
+		}
 
         public string GetWhereClause(IViewMetaModel view)
         {
-
             if (view.FilterElements.Count() == 0) return "";
 
             string where = string.Join("", view.FilterElements.Select(x => string.Format(" AND " + x.ToString())));
