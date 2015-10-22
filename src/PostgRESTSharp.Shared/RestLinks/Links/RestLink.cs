86a97d6d55f824597b6914f5d85015fc63faa0e8
Rowan Landsberg <rowanl@sahomeloans.com>
2015-10-22 14:14:48 +0200

    Convert JavaScriptConverter to a NewtonsoftJsonConverter for the IRestLink
    Still need to add link array support

--- src/PostgRESTSharp.Shared/RestLinks/Links/RestLink.cs
+++ src/PostgRESTSharp.Shared/RestLinks/Links/RestLink.cs
@@ -1,20 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace PostgRESTSharp.Shared
 {
-	public interface IRestLinkUri
-	{
-		string Href { get; }
-	}
-
-	public interface IRestLinkTemplateUri : IRestLinkUri
-	{
-		string Name { get; }
-
-		bool Templated { get; }
-	}
-
+    [JsonConverter(typeof(RestLinkJsonConverter))]
 	public interface IRestLink
 	{
 	}
@@ -27,30 +17,6 @@ namespace PostgRESTSharp.Shared
 	public interface IRestArrayLink : IRestLink
 	{
 		IRestLinkUri[] Uris { get; }
-	}
-
-	public class RestLinkUri : IRestLinkUri
-	{
-		public RestLinkUri(string href)
-		{
-			this.Href = href;
-		}
-
-		public string Href { get; protected set; }
-	}
-
-	public class TemplatedRestLinkUri : RestLinkUri, IRestLinkTemplateUri
-	{
-		public TemplatedRestLinkUri(string href, string name, bool templated)
-			: base(href)
-		{
-			this.Name = name;
-			this.Templated = templated;
-		}
-
-		public bool Templated { get; protected set; }
-
-		public string Name { get; protected set; }
 	}
 
 	public class SimpleRestLink : IRestSimpleLink
---
+++ src/PostgRESTSharp.Shared/RestLinks/RestLinkJsonConverter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PostgRESTSharp.Shared
+{
+    public class RestLinkJsonConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var restLink = value as IRestLink;
+            if (restLink == null)
+            {
+                return;
+            }
+            var simpleRestLink = value as IRestSimpleLink;
+            if (simpleRestLink != null)
+            {
+                var uri = simpleRestLink.Uri;
+                var templatedUri = uri as IRestLinkTemplateUri;
+                if (templatedUri != null)
+                {
+                    Write(writer, templatedUri);
+                }
+                else
+                {
+                    Write(writer, uri);
+                }
+                return;
+            }
+            var array = value as IRestArrayLink;
+            if (array != null)
+            {
+                //TODO: return array of links
+                return;
+            }
+        }
+
+        private void Write(JsonWriter writer, IRestLinkUri uri)
+        {
+            writer.WriteStartObject();
+            {
+                writer.WritePropertyName("href");
+                writer.WriteValue(uri.Href);
+            }
+            writer.WriteEndObject();
+        }
+
+        private void Write(JsonWriter writer, IRestLinkTemplateUri uri)
+        {
+            writer.WriteStartObject();
+            {
+                writer.WritePropertyName("name");
+                writer.WriteValue(uri.Name);
+                writer.WritePropertyName("href");
+                writer.WriteValue(uri.Href);
+                writer.WritePropertyName("templated");
+                writer.WriteValue(uri.Templated);
+            }
+            writer.WriteEndObject();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return null;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(IRestLink).IsAssignableFrom(objectType);
+        }
+    }
+}
