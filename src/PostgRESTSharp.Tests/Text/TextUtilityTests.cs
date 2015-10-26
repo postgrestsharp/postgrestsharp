825b162dcd3f504a97f8394ef0234040365f42f4
Chris Tite <christ@sahomeloans.com>
2015-10-26 11:06:25 +0200

    Include start of test for text utility, start of route naming

---
+++ src/PostgRESTSharp.Tests/Text/TextUtilityTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using PostgRESTSharp.Text;
+
+namespace PostgRESTSharp.Tests.Text
+{
+    [TestFixture]
+    public class TextUtilityTests
+    {
+
+        [Test]
+        public void ToPluralCapitalCase_GivenComplexTableName_ShouldReturnCamelCasePuralisedInReverseEntityOrder()
+        {
+            
+            //arrange
+            var textUtility = new TextUtility();
+            var tableName = "application$mortgageloan";
+
+            //action
+            var result = textUtility.ToPluralCamelCase(tableName);
+
+            //assert
+            Assert.AreEqual("mortgageloanapplications", result);
+
+        }
+         
+    }
+
+}
