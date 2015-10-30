b86b8fcce74f1035646ee6e816545f85273b6229
Rowan Landsberg <rowanl@sahomeloans.com>
2015-10-30 09:28:34 +0200

    Add more exclusion conventions

---
+++ src/PostgRESTSharp/Conventions/ViewConventions/ColumnRemoval/ColumnRemovalForViewWithAccountId.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace PostgRESTSharp.Conventions.ViewConventions.ColumnRemoval
+{
+    public class ColumnRemovalForViewWithAccountId : IColumnRemovalConvention, IImplicitViewConvention
+    {
+        public bool IsMatch(IViewMetaModel metaModel)
+        {
+            return metaModel.Columns.Any(x => x.ColumnName.Equals(ColumnToRemove(), StringComparison.OrdinalIgnoreCase));
+        }
+        
+        public string ColumnToRemove()
+        {
+            return "accountId";
+        }
+    }
+}
---
+++ src/PostgRESTSharp/Conventions/ViewConventions/ColumnRemoval/ColumnRemovalForViewWithResetDateId.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace PostgRESTSharp.Conventions.ViewConventions.ColumnRemoval
+{
+    public class ColumnRemovalForViewWithResetDateId : IColumnRemovalConvention, IImplicitViewConvention
+    {
+        public bool IsMatch(IViewMetaModel metaModel)
+        {
+            return metaModel.Columns.Any(x => x.ColumnName.Equals(ColumnToRemove(), StringComparison.OrdinalIgnoreCase));
+        }
+        
+        public string ColumnToRemove()
+        {
+            return "resetDateId";
+        }
+    }
+}
